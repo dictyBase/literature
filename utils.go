0f0c62e19ea37fa83ebb97ea54797234520a3dcb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-06-18 17:43:28 -0500

    feat: extract pubmed and utils functionalities into separate files
    This commit extracts the PubMed-related functionalities
    and utility functions into separate files, `pubmed.go` and
    `utils.go` respectively. This improves code organization,
    readability, and maintainability by separating concerns.

---
+++ utils.go
@@ -0,0 +1,18 @@
+package main
+
+func Map[T, U any](ts []T, f func(T) U) []U {
+	us := make([]U, len(ts))
+	for i, t := range ts {
+		us[i] = f(t)
+	}
+	return us
+}
+
+func Find[T any](slice []T, predicate func(T) bool) (*T, bool) {
+	for i := range slice {
+		if predicate(slice[i]) {
+			return &slice[i], true
+		}
+	}
+	return nil, false
+}
